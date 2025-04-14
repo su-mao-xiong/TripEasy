514416c805f2a274d205bea623011317ca8510f2
a39635322 <asu39635322@gmail.com>
2025-04-14 10:19:23 +0800

    專案建構

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<!--randomuser的api測試-->
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    this.$http.get('https://randomuser.me/api/')
+      .then((res) => {
+        console.log(res)
+        this.data = res.data.results[0]
+        console.log(this.data)
+      })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
